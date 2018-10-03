a45908c1e8b370113919ba7d437ac975f65341d7
Samir Omarov <samir.omarov@outlook.com>
2018-10-03 08:58:11 -0700

    Initial Commit

---
+++ CenturyWant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CenturyWant
+//
+//  Created by samir omarov on 10/3/18.
+//  Copyright © 2018 samir omarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
